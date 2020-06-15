2c2aa3a1fd01e001a33836606edaba38cd93a77b
Josh Justice <josh.justice@accesscfa.com>
2020-06-15 08:29:48 -0400

    Cast date attributes

--- app/controllers/todos/available.js
+++ app/controllers/todos/available.js
@@ -1,22 +1,13 @@
 import Controller from '@ember/controller';
 import { filter } from '@ember/object/computed';
-import parse from 'date-fns/parse';
-
-const parseFormatString = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
 export default class TodosAvailableController extends Controller {
   @filter('model', function (todo) {
     const now = new Date();
-    console.log({
-      now,
-      name: todo.name,
-      deferredUntil: todo.deferredUntil,
-    });
     return (
       !todo.completedAt &&
       !todo.deletedAt &&
-      (!todo.deferredUntil ||
-        parse(todo.deferredUntil, parseFormatString, new Date()) <= now)
+      (!todo.deferredUntil || todo.deferredUntil <= now)
     );
   })
   filteredTodos;
--- app/models/todo.js
+++ app/models/todo.js
@@ -2,10 +2,10 @@ import Model, { attr } from '@ember-data/model';
 
 export default class TodoModel extends Model {
   @attr name;
-  @attr createdAt;
-  @attr updatedAt;
-  @attr completedAt;
-  @attr deletedAt;
-  @attr deferredAt;
-  @attr deferredUntil;
+  @attr('date') createdAt;
+  @attr('date') updatedAt;
+  @attr('date') completedAt;
+  @attr('date') deletedAt;
+  @attr('date') deferredAt;
+  @attr('date') deferredUntil;
 }
