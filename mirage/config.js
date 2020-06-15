47e4107bfdf903a9236a870da30985419d35425f
Josh Justice <josh.justice@accesscfa.com>
2020-06-15 06:17:18 -0400

    Stub login in mirage

--- mirage/config.js
+++ mirage/config.js
@@ -1,4 +1,13 @@
 export default function () {
+  this.post('/oauth/token', () => {
+    return {
+      access_token: 'fake_access_token',
+      token_type: 'bearer',
+      expires_in: 7200,
+      created_at: 1531855327,
+    };
+  });
+
   // These comments are here to help you get started. Feel free to delete them.
   /*
     Config (with defaults).
