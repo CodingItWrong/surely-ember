645c42910d83e78c3bdc3e4a8973051962f3580f
Josh Justice <josh.justice@accesscfa.com>
2020-06-19 07:07:40 -0400

    Defer until date

--- app/components/todo-detail.js
+++ app/components/todo-detail.js
@@ -3,14 +3,17 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import addDays from 'date-fns/addDays';
 import startOfDay from 'date-fns/startOfDay';
+import parse from 'date-fns/parse';
 
 const BUTTON_SET = {
   ACTIONS: 'actions',
   DEFER: 'defer',
+  DEFER_UNTIL_DATE: 'defer_until_date',
 };
 
 export default class TodoDetailComponent extends Component {
   @tracked buttonSet = BUTTON_SET.ACTIONS;
+  @tracked deferredUntil = null;
 
   @action
   complete() {
@@ -49,18 +52,42 @@ export default class TodoDetailComponent extends Component {
   @action
   cancelDefer() {
     this.buttonSet = BUTTON_SET.ACTIONS;
+    this.deferredUntil = null;
   }
 
   @action
-  async deferOneDay() {
+  deferOneDay() {
     const { todo } = this.args;
 
     const now = new Date();
     const currentDate = todo.deferredUntil || now;
     const tomorrow = startOfDay(addDays(currentDate, 1));
 
-    todo.deferredUntil = tomorrow;
-    todo.deferredAt = now;
+    this.saveDeferUntilDate(tomorrow);
+  }
+
+  @action
+  deferUntilDate() {
+    this.deferredUntil = null;
+    this.buttonSet = BUTTON_SET.DEFER_UNTIL_DATE;
+  }
+
+  @action
+  async saveEnteredDeferredDate() {
+    const deferredUntilDate = parse(
+      this.deferredUntil,
+      'yyyy-MM-dd',
+      new Date(),
+    );
+    await this.saveDeferUntilDate(deferredUntilDate);
+    this.deferredUntil = null;
+  }
+
+  async saveDeferUntilDate(deferredUntil) {
+    const { todo } = this.args;
+
+    todo.deferredUntil = deferredUntil;
+    todo.deferredAt = new Date();
 
     await todo.save();
 
