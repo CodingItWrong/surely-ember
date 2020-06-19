37b137350d7960caa09b05411ae6b26301aaf732
Josh Justice <josh.justice@accesscfa.com>
2020-06-19 07:49:19 -0400

    Format defer without time

---
+++ app/helpers/relative-date.js
@@ -0,0 +1,27 @@
+import { helper } from '@ember/component/helper';
+import formatRelative from 'date-fns/formatRelative';
+import enUS from 'date-fns/locale/en-GB';
+
+// @see https://github.com/date-fns/date-fns/issues/1218
+const formatRelativeLocale = {
+  lastWeek: "'last' eeee",
+  yesterday: "'yesterday'",
+  today: "'today'",
+  tomorrow: "'tomorrow'",
+  nextWeek: "'next' eeee",
+  other: 'MM/dd/yyyy',
+};
+
+const locale = {
+  ...enUS,
+  formatRelative: token => formatRelativeLocale[token],
+};
+
+export default helper(function relativeDate([date]) {
+  if (!date) {
+    return date;
+  }
+
+  const now = new Date();
+  return formatRelative(date, now, { locale });
+});
