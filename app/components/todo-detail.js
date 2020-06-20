217a3fb466e920785f32eddcd56b74d237e6e276
Josh Justice <josh.justice@accesscfa.com>
2020-06-20 06:39:14 -0400

    Edit todo

--- app/components/todo-detail.js
+++ app/components/todo-detail.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import parse from 'date-fns/parse';
+import format from 'date-fns/format';
 
 const BUTTON_SET = {
   ACTIONS: 'actions',
@@ -9,9 +10,15 @@ const BUTTON_SET = {
   DEFER_UNTIL_DATE: 'defer_until_date',
 };
 
+const FORMAT_STRING = 'yyyy-MM-dd';
+
 export default class TodoDetailComponent extends Component {
   @tracked buttonSet = BUTTON_SET.ACTIONS;
   @tracked deferredUntil = null;
+
+  @tracked isEditing = false;
+  @tracked editedName = null;
+  @tracked editedDeferredUntil = null;
 
   @action
   complete() {
@@ -73,11 +80,7 @@ export default class TodoDetailComponent extends Component {
       return;
     }
 
-    const deferredUntilDate = parse(
-      this.deferredUntil,
-      'yyyy-MM-dd',
-      new Date(),
-    );
+    const deferredUntilDate = this.parseDate(this.deferredUntil);
 
     const { todo } = this.args;
     todo.deferUntilDate(deferredUntilDate);
@@ -85,5 +88,40 @@ export default class TodoDetailComponent extends Component {
     this.buttonSet = BUTTON_SET.ACTIONS;
 
     this.deferredUntil = null;
+  }
+
+  @action
+  edit() {
+    const { todo } = this.args;
+    this.editedName = todo.name;
+    this.deferredUntil = this.formatDate(todo.deferredUntil);
+    this.isEditing = true;
+  }
+
+  @action
+  cancelEdit() {
+    this.editedName = null;
+    this.deferredUntil = null;
+    this.isEditing = false;
+  }
+
+  @action
+  async handleSave() {
+    const { todo } = this.args;
+
+    const deferredUntilDate = this.parseDate(this.deferredUntil);
+    todo.name = this.editedName;
+    todo.deferUntilDate(deferredUntilDate);
+    await todo.save();
+
+    this.cancelEdit();
+  }
+
+  parseDate(dateString) {
+    return parse(dateString, FORMAT_STRING, new Date());
+  }
+
+  formatDate(date) {
+    return format(date, FORMAT_STRING);
   }
 }
