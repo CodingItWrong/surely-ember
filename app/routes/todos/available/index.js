a00b73ceff05b2a5daa8be541962a0239967c93a
Josh Justice <josh.justice@accesscfa.com>
2020-06-24 06:11:42 -0400

    Unload before load

--- app/routes/todos/available/index.js
+++ app/routes/todos/available/index.js
@@ -3,7 +3,8 @@ import { action } from '@ember/object';
 
 export default class TodosAvailableIndexRoute extends Route {
   async model() {
-    await this.store.query('todo', {
+    this.store.unloadAll('todo');
+    this.store.query('todo', {
       filter: { status: 'available' },
     });
     return this.store.peekAll('todo');
