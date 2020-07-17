188feecdde2b95fcd277106029ccd79b6cc77ada
Josh Justice <me@codingitwrong.com>
2020-07-17 08:48:28 -0400

    Make side nav open prop explicit

--- app/components/layout.js
+++ app/components/layout.js
@@ -1,3 +1,6 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
-export default class LayoutComponent extends Component {}
+export default class LayoutComponent extends Component {
+  @tracked sideNavOpen = false;
+}
