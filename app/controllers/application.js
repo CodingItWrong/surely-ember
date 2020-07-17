a1803fdae202b65ee164a7510c9d62dda7c0befa
Josh Justice <me@codingitwrong.com>
2020-07-17 06:52:24 -0400

    Remove unused action

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,6 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
   @service session;
-
-  @action
-  async signOut() {
-    this.session.invalidate();
-    this.transitionToRoute('index');
-  }
 }
