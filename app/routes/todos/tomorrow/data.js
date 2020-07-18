80c8fecb2460ff91f1868caeb5f8108ca13278bc
Josh Justice <me@codingitwrong.com>
2020-07-17 20:36:10 -0400

    Add data child route for tomorrow

--- app/routes/todos/tomorrow/data.js
+++ app/routes/todos/tomorrow/data.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
 
-export default class TodosTomorrowIndexRoute extends Route {
+export default class TodosTomorrowDataRoute extends Route {
   async model() {
     await this.store.query('todo', {
       filter: { deferredUntil: 'tomorrow' },
