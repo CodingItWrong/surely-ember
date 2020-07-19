325ee3c7b2e3e9a24a3b80acac316576bdb68549
Josh Justice <me@codingitwrong.com>
2020-07-19 10:29:16 -0400

    Change computed to filter

--- app/controllers/todos/deleted/data.js
+++ app/controllers/todos/deleted/data.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { action, computed } from '@ember/object';
+import { action } from '@ember/object';
+import { filter } from '@ember/object/computed';
 import { tracked } from '@glimmer/tracking';
 import { scrollToTop } from 'surely/utils';
 
@@ -13,9 +14,8 @@ export default class TodosDeletedDataController extends Controller {
     return this.model.meta['page-count'];
   }
 
-  // TODO change to a @filter decorator
-  @computed('model.@each.isDeleted', function () {
-    return this.model.filter(todo => todo.isDeleted);
+  @filter('model.@each.isDeleted', function (todo) {
+    return todo.isDeleted;
   })
   filteredTodos;
 
