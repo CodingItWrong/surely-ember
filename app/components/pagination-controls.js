5d1727183fda62374abe94771e4ab88a284a5036
Josh Justice <me@codingitwrong.com>
2020-07-19 10:26:16 -0400

    Add pagination controls component

---
+++ app/components/pagination-controls.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+
+export default class PaginationControlsComponent extends Component {
+  get anyPages() {
+    return this.args.totalPages > 0;
+  }
+
+  get isFirstPage() {
+    return this.args.pageNumber === 1;
+  }
+
+  get isLastPage() {
+    return this.args.pageNumber >= this.args.totalPages;
+  }
+}
