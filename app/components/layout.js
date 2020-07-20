5cdf43cd26fff7b91c591e4fe777f0a7c007fe45
Josh Justice <me@codingitwrong.com>
2020-07-20 07:05:06 -0400

    Switch to addon for layout components

--- app/components/layout.js
+++
@@ -1,6 +0,0 @@
-import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-
-export default class LayoutComponent extends Component {
-  @tracked sideNavOpen = false;
-}
