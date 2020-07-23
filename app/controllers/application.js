4161655e2d0b932fefcfa9f4c4c458b90f17bb68
Josh Justice <me@codingitwrong.com>
2020-07-23 07:06:07 -0400

    Allow unauthenticated routes

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,17 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
+const unauthenticatedRoutes = ['user.new'];
+
 export default class ApplicationController extends Controller {
+  @service router;
   @service session;
+
+  get currentRouteIsUnauthenticated() {
+    return unauthenticatedRoutes.includes(this.router.currentRouteName);
+  }
+
+  get routeRequiresAuthentication() {
+    return !this.currentRouteIsUnauthenticated && !this.session.isAuthenticated;
+  }
 }
