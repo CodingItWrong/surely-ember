80853afe48641a5c0aa0a1145aff5c9e4d575546
Josh Justice <me@codingitwrong.com>
2020-07-23 07:49:56 -0400

    Get passing acceptance success test

---
+++ tests/acceptance/sign-up-test.js
@@ -0,0 +1,23 @@
+import { module, test } from 'qunit';
+import { visit, click, fillIn, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | sign up', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('creating a new account', async function (assert) {
+    await visit('/');
+
+    await click('[data-test-sign-up-button]');
+    await fillIn('[data-test-email-field] input', 'example@example.com');
+    const password = 'password';
+    await fillIn('[data-test-password-field] input', password);
+    await fillIn('[data-test-password-confirmation-field] input', password);
+    await click('[data-test-sign-up-confirm-button]');
+
+    assert.equal(currentURL(), '/todos/available');
+    assert.dom('[data-test-new-todo-field]').exists();
+  });
+});
