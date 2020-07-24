6b1b1d7e179e9fb0e60daa9972a534212703a076
Josh Justice <me@codingitwrong.com>
2020-07-24 08:16:29 -0400

    Comment lint exceptions

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,8 +4,11 @@ module.exports = {
   extends: 'octane',
   rules: {
     'no-action': false, // ember-paper requires it
+    'no-curly-component-invocation': {
+      allow: ['day-of-week-days-from'], // helper
+    },
     'no-implicit-this': {
-      allow: ['tomorrow'],
+      allow: ['tomorrow'], // helper
     },
   },
 };
