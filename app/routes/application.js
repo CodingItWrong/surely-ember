917f1b906595d01f58f6951e8a2b203f22e66115
Josh Justice <me@codingitwrong.com>
2020-07-29 08:41:45 -0400

    Add counts to sidebar

---
+++ app/routes/application.js
@@ -0,0 +1,15 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class ApplicationRoute extends Route {
+  @service session;
+
+  async model() {
+    if (this.session.isAuthenticated) {
+      await this.store.query('todo', {
+        filter: { status: 'available,tomorrow' },
+      });
+      return this.store.peekAll('todo');
+    }
+  }
+}
