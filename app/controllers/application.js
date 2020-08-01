5f79b801e45ddddd93fae6babaad249b7400a4aa
Josh Justice <me@codingitwrong.com>
2020-08-01 06:57:17 -0400

    Freeze sidebar counts with service

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 import { action } from '@ember/object';
 import { count, prop } from 'surely/utils';
 
@@ -9,15 +8,22 @@ const unauthenticatedRoutes = ['user.new'];
 export default class ApplicationController extends Controller {
   @service router;
   @service session;
+  @service sidebarCounts;
 
-  @computed('model.@each.isAvailable')
   get availableTodoCount() {
-    return count(this.model, prop('isAvailable'));
+    if (this.sidebarCounts.availableCount !== null) {
+      return this.sidebarCounts.availableCount;
+    } else {
+      return count(this.model, prop('isAvailable'));
+    }
   }
 
-  @computed('model.@each.isTomorrow')
   get tomorrowTodoCount() {
-    return count(this.model, prop('isTomorrow'));
+    if (this.sidebarCounts.tomorrowCount !== null) {
+      return this.sidebarCounts.tomorrowCount;
+    } else {
+      return count(this.model, prop('isTomorrow'));
+    }
   }
 
   get currentRouteIsUnauthenticated() {
