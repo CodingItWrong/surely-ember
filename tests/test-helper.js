0e164db55ce0f6ff7a18c22181dc2a3996d42caa
Josh Justice <me@codingitwrong.com>
2020-08-01 11:45:56 -0400

    v3.19.0...v3.20.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'surely/app';
+import config from 'surely/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
