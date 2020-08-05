c73458b23c97d4870b4154370c9cb87a103eb7d3
Josh Justice <me@codingitwrong.com>
2020-08-05 08:51:21 -0400

    Test success case for form

--- tests/integration/components/new-todo-form-test.js
+++ tests/integration/components/new-todo-form-test.js
@@ -1,13 +1,41 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, triggerEvent } from '@ember/test-helpers';
+import { render, fillIn, triggerEvent, pauseTest } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import sinon from 'sinon';
 
 module('Integration | Component | new-todo-form', function (hooks) {
   setupRenderingTest(hooks);
 
-  module('when submitting with no todo name', () => {
+  module('success', function (hooks) {
+    const todoName = 'Todo Name';
+
+    let handleCreate;
+
+    hooks.beforeEach(async function () {
+      handleCreate = sinon.stub().resolves();
+
+      this.set('handleCreate', handleCreate);
+      await render(hbs`<NewTodoForm @handleCreate={{handleCreate}} />`);
+
+      await fillIn('[data-test-new-todo-field] input', todoName);
+      await triggerEvent('[data-test-new-todo-form]', 'submit');
+    });
+
+    test('it passes the todo name to handleCreate', function (assert) {
+      assert.ok(handleCreate.calledWith(todoName));
+    });
+
+    test('it clears the text field', function (assert) {
+      assert.dom('[data-test-new-todo-field] input').hasValue('');
+    });
+
+    test('it does not display an error', function (assert) {
+      assert.dom('[data-test-error]').doesNotExist();
+    });
+  });
+
+  module('when submitting with no todo name', function (hooks) {
     let onAdd;
 
     hooks.beforeEach(async function () {
