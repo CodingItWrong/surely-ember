d6fab4d90c7e2cdc62864bf4275529f17cf30f7b
Josh Justice <me@codingitwrong.com>
2020-08-05 08:07:53 -0400

    Remove Ember Data from new todo form

--- app/utils.js
+++ app/utils.js
@@ -1,3 +1,10 @@
+import addDays from 'date-fns/addDays';
+import startOfDay from 'date-fns/startOfDay';
+
 export const scrollToTop = () => window.scrollTo(0, 0);
 export const count = (array, test) => array.filter(test).length;
 export const prop = name => object => object[name];
+export const tomorrow = () => {
+  const now = new Date();
+  return startOfDay(addDays(now, 1));
+};
