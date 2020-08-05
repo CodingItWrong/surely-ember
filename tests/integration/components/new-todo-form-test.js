b5d085d680708679e5426ba04956ab8b71923c98
Josh Justice <me@codingitwrong.com>
2020-08-05 09:04:34 -0400

    SQUASH remove unused pauseTest

--- tests/integration/components/new-todo-form-test.js
+++ tests/integration/components/new-todo-form-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, fillIn, triggerEvent, pauseTest } from '@ember/test-helpers';
+import { render, fillIn, triggerEvent } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import sinon from 'sinon';
 
