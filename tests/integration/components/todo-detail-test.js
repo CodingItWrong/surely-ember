6be6affa18102b33cfe300561ef2331c6a52b771
Josh Justice <me@codingitwrong.com>
2020-08-06 14:53:49 -0400

    Assert on completed, deleted, and future todos

--- tests/integration/components/todo-detail-test.js
+++ tests/integration/components/todo-detail-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, pauseTest } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import set from 'date-fns/set';
 import addDays from 'date-fns/addDays';
@@ -37,6 +37,70 @@ module('Integration | Component | todo-detail', function (hooks) {
         .dom('[data-test-deferred-until]')
         .hasText('Deferred until yesterday');
       assert.dom('[data-test-created-at]').hasText('Created today at 1:23 AM');
+    });
+  });
+
+  module('future todo', function (hooks) {
+    test('it displays todo info', async function (assert) {
+      const now = new Date();
+      const deferredUntil = addDays(now, 1);
+      const todo = {
+        id: 1,
+        deferredUntil,
+      };
+
+      this.set('todo', todo);
+      await render(hbs`<TodoDetail @todo={{todo}} />`);
+
+      assert
+        .dom('[data-test-deferred-until]')
+        .hasText('Deferred until tomorrow');
+    });
+  });
+
+  module('completed todo', function (hooks) {
+    test('it displays todo info', async function (assert) {
+      const now = new Date();
+      const completedAt = set(addDays(now, -1), {
+        hours: 1,
+        minutes: 23,
+        seconds: 0,
+      });
+      const todo = {
+        id: 1,
+        isCompleted: true,
+        completedAt,
+      };
+
+      this.set('todo', todo);
+      await render(hbs`<TodoDetail @todo={{todo}} />`);
+
+      assert
+        .dom('[data-test-completed-at]')
+        .hasText('Completed yesterday at 1:23 AM');
+    });
+  });
+
+  module('deleted todo', function (hooks) {
+    test('it displays todo info', async function (assert) {
+      const now = new Date();
+      const deletedAt = set(addDays(now, -1), {
+        hours: 1,
+        minutes: 23,
+        seconds: 0,
+      });
+      const todo = {
+        id: 1,
+        isDeleted: true,
+        deletedAt,
+      };
+
+      this.set('todo', todo);
+      await render(hbs`<TodoDetail @todo={{todo}} />`);
+
+      assert
+        .dom('[data-test-deleted-at]')
+        .hasText('Deleted yesterday at 1:23 AM');
     });
   });
 });
