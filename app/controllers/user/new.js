c78cd748a6b211d8fc7a3e4ff72a68c7d8bc5759
Josh Justice <me@codingitwrong.com>
2020-08-06 07:44:14 -0400

    Extract sign up logic out of sign up form

--- app/controllers/user/new.js
+++ app/controllers/user/new.js
@@ -3,13 +3,24 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class UserNewController extends Controller {
+  @service session;
   @service router;
 
   @action goToSignIn() {
     this.router.transitionTo('index');
   }
 
-  @action handleSignUp() {
+  @action
+  async handleSignUp({ email, password }) {
+    // create user
+    const user = this.store.createRecord('user', { email, password });
+    await user.save();
+
+    // sign in
+    this.store.unloadAll('user');
+    await this.session.authenticate('authenticator:oauth', email, password);
+
+    // navigate to available todos
     this.send('refreshApplicationModel');
     this.router.transitionTo('todos.available');
   }
