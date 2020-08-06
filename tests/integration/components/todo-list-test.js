8929bc438a773b8289ca781d76be643835dcd307
Josh Justice <me@codingitwrong.com>
2020-08-06 08:12:25 -0400

    Test rendering and clicking todos

--- tests/integration/components/todo-list-test.js
+++ tests/integration/components/todo-list-test.js
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
 
 module('Integration | Component | todo-list', function (hooks) {
   setupRenderingTest(hooks);
@@ -21,6 +22,39 @@ module('Integration | Component | todo-list', function (hooks) {
       assert
         .dom('[data-test-error-message]')
         .hasText('An error occurred loading todos.');
+    });
+  });
+
+  module('individual todos', function (hooks) {
+    const todos = [
+      { id: 1, name: 'Todo 1' },
+      { id: 2, name: 'Todo 2' },
+    ];
+
+    let handleChooseTodo;
+
+    hooks.beforeEach(async function () {
+      handleChooseTodo = sinon.spy();
+
+      this.set('todos', todos);
+      this.set('handleChooseTodo', handleChooseTodo);
+      await render(
+        hbs`<TodoList @todos={{todos}} @onChooseTodo={{handleChooseTodo}} />`,
+      );
+    });
+
+    test('it renders each todo', async function (assert) {
+      assert.dom('[data-test-todo-name]').exists({ count: 2 });
+      assert.dom('[data-test-todo-name]').hasText(todos[0].name);
+    });
+
+    test('it calls onChooseTodo when clicking a todo', async function (assert) {
+      await click('[data-test-todo] button');
+      assert.equal(
+        handleChooseTodo.getCall(0).args,
+        todos[0],
+        'passed todo to onChooseTodo',
+      );
     });
   });
 });
