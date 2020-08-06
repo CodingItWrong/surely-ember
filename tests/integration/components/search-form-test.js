219d3885cfb70d38e86a9d94637f7f3c27ab7af6
Josh Justice <me@codingitwrong.com>
2020-08-05 20:06:46 -0400

    Test search form

---
+++ tests/integration/components/search-form-test.js
@@ -0,0 +1,32 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, fillIn, triggerEvent } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
+
+module('Integration | Component | search-form', function (hooks) {
+  setupRenderingTest(hooks);
+
+  module('on submit', function (hooks) {
+    const searchText = 'Search Text';
+    let onSearch;
+
+    hooks.beforeEach(async function () {
+      onSearch = sinon.spy();
+
+      this.set('onSearch', onSearch);
+      await render(hbs`<SearchForm @onSearch={{onSearch}} />`);
+
+      await fillIn('[data-test-search-field] input', searchText);
+      await triggerEvent('[data-test-search-form]', 'submit');
+    });
+
+    test('it calls onSearch with the search text', async function (assert) {
+      assert.ok(onSearch.calledWith(searchText));
+    });
+
+    test('it does not clear the text field', async function (assert) {
+      assert.dom('[data-test-search-field] input').hasValue(searchText);
+    });
+  });
+});
