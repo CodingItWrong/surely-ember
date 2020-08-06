a98f6a53ce531f2d7bf6f3c6a3bf5c5c2ea80425
Josh Justice <me@codingitwrong.com>
2020-08-06 07:49:23 -0400

    Test sign up form success

--- tests/integration/components/sign-up-form-test.js
+++ tests/integration/components/sign-up-form-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, fillIn, triggerEvent } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
 
 module('Integration | Component | sign-up-form', function (hooks) {
   setupRenderingTest(hooks);
@@ -54,6 +55,36 @@ module('Integration | Component | sign-up-form', function (hooks) {
       assert
         .dom('[data-test-error-message]')
         .hasText('An error occurred while signing up.');
+    });
+  });
+
+  module('sign up success', function (hooks) {
+    const email = 'email@example.com';
+    const password = 'password';
+
+    let handleSignUp;
+
+    hooks.beforeEach(async function () {
+      handleSignUp = sinon.stub().resolves();
+      this.set('handleSignUp', handleSignUp);
+      await render(hbs`<SignUpForm @signUp={{handleSignUp}}/>`);
+
+      await fillIn('[data-test-email-field] input', email);
+      await fillIn('[data-test-password-field] input', password);
+      await fillIn('[data-test-password-confirmation-field] input', password);
+      await triggerEvent('[data-test-sign-up-form]', 'submit');
+    });
+
+    test('it passes the credentials to signUp', async function (assert) {
+      assert.deepEqual(
+        handleSignUp.getCall(0).args,
+        [{ email, password }],
+        'handleSignup called with credentials',
+      );
+    });
+
+    test('it does not display an error message', async function (assert) {
+      assert.dom('[data-test-error-message]').hasText('');
     });
   });
 });
