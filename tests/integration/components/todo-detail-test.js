69f33cd5282e8025e52a4004b0e5ecb67e04293e
Josh Justice <me@codingitwrong.com>
2020-08-07 06:38:09 -0400

    Test deleting, undeleting, and uncompleting

--- tests/integration/components/todo-detail-test.js
+++ tests/integration/components/todo-detail-test.js
@@ -98,6 +98,65 @@ module('Integration | Component | todo-detail', function (hooks) {
         });
       });
     });
+
+    module('deleting', function (hooks) {
+      module('on success', function (hooks) {
+        let todo;
+        let onHandle;
+
+        hooks.beforeEach(async function () {
+          todo = {
+            save: sinon.stub().resolves(),
+          };
+          onHandle = sinon.spy();
+          this.set('todo', todo);
+          this.set('onHandle', onHandle);
+          await render(
+            hbs`<TodoDetail @todo={{todo}} @onHandle={{onHandle}} />`,
+          );
+
+          await click('[data-test-delete-button]');
+        });
+
+        test('it marks the todo deleted', function (assert) {
+          assert.ok(todo.deletedAt instanceof Date, 'deleted date set');
+          assert.ok(todo.save.calledOnce, 'save called');
+        });
+
+        test('it calls onHandle', function (assert) {
+          assert.ok(onHandle.calledOnce, 'calls onHandle');
+        });
+      });
+
+      module('on error', function (hooks) {
+        let todo;
+        let onHandle;
+
+        hooks.beforeEach(async function () {
+          todo = {
+            save: sinon.stub().rejects(),
+          };
+          onHandle = sinon.spy();
+          this.set('todo', todo);
+          this.set('onHandle', onHandle);
+          await render(
+            hbs`<TodoDetail @todo={{todo}} @onHandle={{onHandle}} />`,
+          );
+
+          await click('[data-test-delete-button]');
+        });
+
+        test('it displays an error', function (assert) {
+          assert
+            .dom('[data-test-error-message]')
+            .hasText('An error occurred while deleting the todo.');
+        });
+
+        test('it does not call onHandle', function (assert) {
+          assert.ok(onHandle.notCalled, 'onHandle not called');
+        });
+      });
+    });
   });
 
   module('future todo', function (hooks) {
@@ -139,6 +198,65 @@ module('Integration | Component | todo-detail', function (hooks) {
         .dom('[data-test-completed-at]')
         .hasText('Completed yesterday at 1:23 AM');
     });
+
+    module('uncompleting', function (hooks) {
+      module('on success', function (hooks) {
+        let todo;
+        let onHandle;
+
+        hooks.beforeEach(async function () {
+          todo = {
+            isCompleted: true,
+            completedAt: new Date(),
+            save: sinon.stub().resolves(),
+          };
+          onHandle = sinon.spy();
+          this.set('todo', todo);
+          this.set('onHandle', onHandle);
+          await render(
+            hbs`<TodoDetail @todo={{todo}} @onHandle={{onHandle}} />`,
+          );
+
+          await click('[data-test-uncomplete-button]');
+        });
+
+        test('it marks the todo saved', function (assert) {
+          assert.equal(todo.completedAt, null, 'completed date set to null');
+          assert.ok(todo.save.calledOnce, 'save called');
+        });
+
+        test('it does not call onHandle', function (assert) {
+          assert.ok(onHandle.notCalled, 'does not call onHandle');
+        });
+      });
+
+      module('on error', function (hooks) {
+        let todo;
+        let onHandle;
+
+        hooks.beforeEach(async function () {
+          todo = {
+            isCompleted: true,
+            completedAt: new Date(),
+            save: sinon.stub().rejects(),
+          };
+          onHandle = sinon.spy();
+          this.set('todo', todo);
+          this.set('onHandle', onHandle);
+          await render(
+            hbs`<TodoDetail @todo={{todo}} @onHandle={{onHandle}} />`,
+          );
+
+          await click('[data-test-uncomplete-button]');
+        });
+
+        test('it displays an error', function (assert) {
+          assert
+            .dom('[data-test-error-message]')
+            .hasText('An error occurred while uncompleting the todo.');
+        });
+      });
+    });
   });
 
   module('deleted todo', function (hooks) {
@@ -161,6 +279,65 @@ module('Integration | Component | todo-detail', function (hooks) {
       assert
         .dom('[data-test-deleted-at]')
         .hasText('Deleted yesterday at 1:23 AM');
+    });
+
+    module('undeleting', function (hooks) {
+      module('on success', function (hooks) {
+        let todo;
+        let onHandle;
+
+        hooks.beforeEach(async function () {
+          todo = {
+            isDeleted: true,
+            deletedAt: new Date(),
+            save: sinon.stub().resolves(),
+          };
+          onHandle = sinon.spy();
+          this.set('todo', todo);
+          this.set('onHandle', onHandle);
+          await render(
+            hbs`<TodoDetail @todo={{todo}} @onHandle={{onHandle}} />`,
+          );
+
+          await click('[data-test-undelete-button]');
+        });
+
+        test('it marks the todo undeleted', function (assert) {
+          assert.equal(todo.deletedAt, null, 'deleted date set to null');
+          assert.ok(todo.save.calledOnce, 'save called');
+        });
+
+        test('it does not call onHandle', function (assert) {
+          assert.ok(onHandle.notCalled, 'onHandle not called');
+        });
+      });
+
+      module('on error', function (hooks) {
+        let todo;
+        let onHandle;
+
+        hooks.beforeEach(async function () {
+          todo = {
+            isDeleted: true,
+            deletedAt: new Date(),
+            save: sinon.stub().rejects(),
+          };
+          onHandle = sinon.spy();
+          this.set('todo', todo);
+          this.set('onHandle', onHandle);
+          await render(
+            hbs`<TodoDetail @todo={{todo}} @onHandle={{onHandle}} />`,
+          );
+
+          await click('[data-test-undelete-button]');
+        });
+
+        test('it displays an error', function (assert) {
+          assert
+            .dom('[data-test-error-message]')
+            .hasText('An error occurred while undeleting the todo.');
+        });
+      });
     });
   });
 });
