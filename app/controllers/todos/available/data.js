ac59284ede57db39e79c6e4da5bbd71ac5a681d5
Josh Justice <me@codingitwrong.com>
2020-08-08 08:16:59 -0400

    Group available list by category

--- app/controllers/todos/available/data.js
+++ app/controllers/todos/available/data.js
@@ -1,8 +1,9 @@
 import Controller from '@ember/controller';
 import { sort, filter } from '@ember/object/computed';
-import { action } from '@ember/object';
+import { action, computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { scrollToTop } from 'surely/utils';
+import groupBy from 'lodash-es/groupBy';
 
 export default class TodosAvailableDataController extends Controller {
   @service router;
@@ -16,6 +17,21 @@ export default class TodosAvailableDataController extends Controller {
 
   @sort('filteredTodos', 'sortPropertiesAlphabetical')
   sortedTodos;
+
+  @computed('sortedTodos.@each.deferredUntil', function () {
+    const todosWithCategoryName = this.sortedTodos.map(todo => ({
+      todo,
+      categoryName: todo.category.get('name'),
+    }));
+    const groupsObject = groupBy(todosWithCategoryName, 'categoryName');
+    return Object.entries(groupsObject).map(([, todoWrappers]) => {
+      return {
+        name: todoWrappers[0].categoryName ?? 'No Category',
+        todos: todoWrappers.map(wrapper => wrapper.todo),
+      };
+    });
+  })
+  todoGroups;
 
   @action
   goToList() {
