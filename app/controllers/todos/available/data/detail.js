de45ccd07a746e0be1754aeb51404206001a33d3
Josh Justice <me@codingitwrong.com>
2020-08-08 09:44:07 -0400

    Sort categories for form

--- app/controllers/todos/available/data/detail.js
+++ app/controllers/todos/available/data/detail.js
@@ -1,9 +1,15 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { sort } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default class TodosAvailableDataDetailController extends Controller {
   @service router;
+
+  sortPropertiesAlphabetical = Object.freeze(['name:asc']);
+
+  @sort('model.categories', 'sortPropertiesAlphabetical')
+  sortedCategories;
 
   @action
   goToAvailable() {
