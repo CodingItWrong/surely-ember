8c0dcbb51419375d21f392db69c3a29d51bc0d22
Josh Justice <me@codingitwrong.com>
2020-08-08 19:00:26 -0400

    Test adding a category

---
+++ tests/acceptance/managing-categories-test.js
@@ -0,0 +1,31 @@
+import { module, test } from 'qunit';
+import {
+  visit,
+  click,
+  fillIn,
+  triggerEvent,
+  // eslint-disable-next-line no-unused-vars
+  pauseTest,
+} from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { authenticateSession } from 'ember-simple-auth/test-support';
+
+module('Acceptance | managing categories', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('it allows adding, editing, and deleting categories', async function (assert) {
+    await authenticateSession({ access_token: 'ABC123' });
+
+    await visit('/');
+    await click('[data-test-categories] button');
+
+    // add category
+    const categoryName = 'New Category';
+    await click('[data-test-add-button]');
+    await fillIn('[data-test-category-name-field] textarea', categoryName);
+    await triggerEvent('[data-test-category-edit-form]', 'submit');
+    assert.dom('[data-test-category-name]').hasText(categoryName);
+  });
+});
