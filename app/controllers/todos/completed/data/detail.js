4bfb4c551656e6a4a8ca9a392aeda172b2ba4f92
Josh Justice <me@codingitwrong.com>
2020-08-08 14:08:56 -0400

    Load category list on all detail routes

--- app/controllers/todos/completed/data/detail.js
+++ app/controllers/todos/completed/data/detail.js
@@ -1,9 +1,15 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { sort } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default class TodosCompletedDataDetailController extends Controller {
   @service router;
+
+  sortPropertiesSortOrderField = Object.freeze(['sortOrder:asc,name:asc']);
+
+  @sort('model.categories', 'sortPropertiesSortOrderField')
+  sortedCategories;
 
   @action
   goToCompleted() {
