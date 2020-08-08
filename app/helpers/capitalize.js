2a185e795b82b66e2cdae92213fba30e6bcda0bc
Josh Justice <me@codingitwrong.com>
2020-08-08 08:14:15 -0400

    Expose helpers as plain functions

--- app/helpers/capitalize.js
+++ app/helpers/capitalize.js
@@ -1,9 +1,13 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function capitalize([string]) {
+export const capitalizeFn = string => {
   if (typeof string !== 'string') {
     return string;
   }
 
   return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export default helper(function capitalize([string]) {
+  return capitalizeFn(string);
 });
