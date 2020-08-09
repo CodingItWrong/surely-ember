deb54181a415b170df900736037da19d8cd01eb2
Josh Justice <me@codingitwrong.com>
2020-08-09 07:40:38 -0400

    Set up unit test wrapper

---
+++ tests/unit/utils-test.js
@@ -0,0 +1,9 @@
+import { module, test } from 'qunit';
+
+module('Unit | utils', function () {
+  module('groupTodosByCategorySorted', function () {
+    test('it works', function (assert) {
+      assert.ok(true);
+    });
+  });
+});
