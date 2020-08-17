4ab0f0ab59a5d15cf7f824b689065536f1c6b7be
Josh Justice <me@codingitwrong.com>
2020-08-17 10:48:50 -0400

    Test cancelling

--- tests/integration/components/category-detail-test.js
+++ tests/integration/components/category-detail-test.js
@@ -1,7 +1,8 @@
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupRenderingTest } from 'ember-qunit';
 import { module, test } from 'qunit';
+import sinon from 'sinon';
 
 module('Integration | Component | category-detail', function (hooks) {
   setupRenderingTest(hooks);
@@ -10,6 +11,27 @@ module('Integration | Component | category-detail', function (hooks) {
     test('it displays an error message', async function (assert) {
       await render(hbs`<CategoryDetail @error={{true}} />`);
       assert.dom('[data-test-error-message]').hasText('Error loading category');
+    });
+  });
+
+  module('editing', function () {
+    module('on cancel', function (hooks) {
+      let onCancel;
+
+      hooks.beforeEach(async function () {
+        onCancel = sinon.spy();
+        this.set('category', {});
+        this.set('onCancel', onCancel);
+        await render(
+          hbs`<CategoryDetail @category={{category}} @onCancel={{onCancel}} />`,
+        );
+
+        await click('[data-test-cancel-edit-button]');
+      });
+
+      test('it calls onCancel', async function (assert) {
+        assert.ok(onCancel.calledOnce, 'calls onCancel');
+      });
     });
   });
 });
