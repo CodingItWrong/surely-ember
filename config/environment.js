b4446189598ff7df104866cada26bcee3cfbc52c
Josh Justice <me@codingitwrong.com>
2020-08-19 20:04:32 -0400

    Switch prod domain to owned domain

--- config/environment.js
+++ config/environment.js
@@ -47,7 +47,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.apiHost = 'https://surely-api.herokuapp.com';
+    ENV.apiHost = 'https://api.surelytodo.com';
   }
 
   return ENV;
