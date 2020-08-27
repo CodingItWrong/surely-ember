a3fff3dd497b30206754a405e1338c3316528726
Josh Justice <me@codingitwrong.com>
2020-08-27 11:25:46 -0400

    Remove absolute dates

--- tests/unit/helpers/defer-date-test.js
+++ tests/unit/helpers/defer-date-test.js
@@ -6,48 +6,42 @@ import { deferDateFn } from 'surely/helpers/defer-date';
 module('Unit | Helper | defer-date', function (hooks) {
   setupTest(hooks);
 
-  // TODO: make these work indefinitely in the future, maybe just pick Jan of next year and ignore days of week
-  const mondayNineAM = new Date(2021, 0, 6, 9, 0, 0);
-  const tuesdayNineAM = new Date(2021, 0, 7, 9, 0, 0);
-  const wednesdayNineAM = new Date(2021, 0, 8, 9, 0, 0);
+  const now = new Date();
+  const yesterday = addDays(now, -1);
+  const tomorrow = addDays(now, 1);
+  const twoDaysFromNow = addDays(now, 2);
+  const threeDaysFromNow = addDays(now, 3);
 
   test('it can get the next day', function (assert) {
     let result = deferDateFn({
-      start: mondayNineAM,
+      start: tomorrow,
       days: 1,
     });
-    assert.deepEqual(result, tuesdayNineAM);
+    assert.deepEqual(result, twoDaysFromNow);
   });
 
-  test('it can get two days from now', function (assert) {
+  test('it can advance multiple days', function (assert) {
     let result = deferDateFn({
-      start: mondayNineAM,
+      start: tomorrow,
       days: 2,
     });
-    assert.deepEqual(result, wednesdayNineAM);
+    assert.deepEqual(result, threeDaysFromNow);
   });
 
   test('it adjusts for the start date', function (assert) {
     let result = deferDateFn({
-      start: tuesdayNineAM,
+      start: twoDaysFromNow,
       days: 1,
     });
-    assert.deepEqual(result, wednesdayNineAM);
+    assert.deepEqual(result, threeDaysFromNow);
   });
 
   test('it defaults to start on the current day', function (assert) {
-    const now = new Date();
-    const tomorrow = addDays(now, 1);
-
     let result = deferDateFn({ now, days: 1 });
     assert.deepEqual(result, tomorrow);
   });
 
   test('it starts from the current date when the passed-in date is in the past', function (assert) {
-    const now = new Date();
-    const yesterday = addDays(now, -1);
-    const tomorrow = addDays(now, 1);
-
     let result = deferDateFn({ now, start: yesterday, days: 1 });
     assert.deepEqual(result, tomorrow);
   });
