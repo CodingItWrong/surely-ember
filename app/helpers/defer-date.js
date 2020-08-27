bcc5a042b6926da263868dc916662a4c3065b14d
Josh Justice <me@codingitwrong.com>
2020-08-27 09:06:35 -0400

    Default to now if in past

--- app/helpers/defer-date.js
+++ app/helpers/defer-date.js
@@ -2,7 +2,16 @@ import { helper } from '@ember/component/helper';
 import addDays from 'date-fns/addDays';
 
 export function deferDateFn({ start, days }) {
-  const startToUse = start ?? new Date();
+  const now = new Date();
+  let startToUse;
+  if (!start || start < now) {
+    // no future defer date: defer 1 day from now
+    startToUse = now;
+  } else {
+    // already future: defer one additional day
+    startToUse = start;
+  }
+
   return addDays(startToUse, days);
 }
 
