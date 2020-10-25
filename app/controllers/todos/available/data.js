79cc26d200f3844346a3124c0a1c8ad718395a44
Josh Justice <me@codingitwrong.com>
2020-10-25 10:31:01 -0400

    Remove unused computed properties

--- app/controllers/todos/available/data.js
+++ app/controllers/todos/available/data.js
@@ -1,22 +1,11 @@
 import Controller from '@ember/controller';
 import { action, computed } from '@ember/object';
-import { filter, sort } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import Todos from 'surely/lib/todos';
 import { scrollToTop } from 'surely/utils';
 
 export default class TodosAvailableDataController extends Controller {
   @service router;
-
-  sortPropertiesAlphabetical = Object.freeze(['name:asc']);
-
-  @filter('model.@each.{id,isAvailable}', function (todo) {
-    return todo.id && todo.isAvailable;
-  })
-  filteredTodos;
-
-  @sort('filteredTodos', 'sortPropertiesAlphabetical')
-  sortedTodos;
 
   @computed('model.@each.{id,isAvailable,name,category}', 'store', function () {
     const todos = Todos.forStore(this.store);
