c8614273ca5244cef009a2fcf2bc23e030deecbf
Josh Justice <me@codingitwrong.com>
2020-10-25 07:42:08 -0400

    Use Todos class in route

--- app/routes/todos/available/data.js
+++ app/routes/todos/available/data.js
@@ -1,19 +1,17 @@
 import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import Todos from 'surely/lib/todos';
 
 export default class TodosAvailableDataRoute extends Route {
   @service sidebarCounts;
 
   async model() {
-    const oldTodos = this.store.peekAll('todo');
+    const todos = Todos.forStore(this.store);
+    const oldTodos = todos.all;
     return this.sidebarCounts.freezeDuring(oldTodos, async () => {
-      this.store.unloadAll();
-      await this.store.query('todo', {
-        filter: { status: 'available,tomorrow' },
-        include: 'category',
-      });
-      return this.store.peekAll('todo');
+      await todos.loadAvailable();
+      return todos.all;
     });
   }
 
