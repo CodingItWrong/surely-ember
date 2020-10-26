900f984f991d11d0aa5f7f721a89418131d4e447
Josh Justice <me@codingitwrong.com>
2020-10-26 09:19:24 -0400

    Move record creation into new todo form

--- app/controllers/todos/tomorrow.js
+++ app/controllers/todos/tomorrow.js
@@ -1,18 +1,12 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { tomorrow } from 'surely/utils';
 
 export default class TodosTomorrowController extends Controller {
   @service router;
 
   @action
-  async handleCreate(todoName) {
-    const todo = this.store.createRecord('todo', {
-      name: todoName,
-      deferredUntil: tomorrow(),
-    });
-    await todo.save();
+  handleCreate() {
     this.router.transitionTo('todos.tomorrow.data');
   }
 }
