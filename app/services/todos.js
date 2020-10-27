8a6bad12bc2c22eaac6cb9113f2c3b8eef82942c
Josh Justice <me@codingitwrong.com>
2020-10-26 20:29:42 -0400

    Add non-live records behind feature flag

--- app/services/todos.js
+++ app/services/todos.js
@@ -1,8 +1,14 @@
+import { A } from '@ember/array';
 import Service, { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 import Todos from 'surely/lib/todos';
+
+const EMBER_DATA_LIVE_RECORDS = false;
 
 export default class TodosService extends Service {
   @service store;
+
+  @tracked _all = A([]);
 
   get todos() {
     if (!this._todos) {
@@ -11,15 +17,28 @@ export default class TodosService extends Service {
     return this._todos;
   }
 
-  loadAvailable() {
-    return this.todos.loadAvailable();
+  async loadAvailable() {
+    await this.todos.loadAvailable();
+    this._refreshAll();
   }
 
-  create(attrs) {
-    return this.todos.create(attrs);
+  async create(attrs) {
+    const record = await this.todos.create(attrs);
+    this._refreshAll();
+    return record;
+  }
+
+  _refreshAll() {
+    if (!EMBER_DATA_LIVE_RECORDS) {
+      this._all.setObjects(this.todos.all);
+    }
   }
 
   get all() {
-    return this.todos.all;
+    if (EMBER_DATA_LIVE_RECORDS) {
+      return this.todos.all;
+    } else {
+      return this._all;
+    }
   }
 }
