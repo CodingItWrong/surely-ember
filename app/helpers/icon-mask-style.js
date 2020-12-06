6b2683d21c4f8a1683617ead2d53ed8ebb685a51
Josh Justice <me@codingitwrong.com>
2020-12-06 15:56:27 -0500

    Fix style attribute warning

--- app/helpers/icon-mask-style.js
+++ app/helpers/icon-mask-style.js
@@ -1,6 +1,7 @@
 import { helper } from '@ember/component/helper';
+import { htmlSafe } from '@ember/template';
 
 export default helper(function iconMaskStyle([icon]) {
   const url = `url(/icons/${icon}.svg)`;
-  return `mask-image: ${url}; -webkit-mask-image: ${url}`;
+  return htmlSafe(`mask-image: ${url}; -webkit-mask-image: ${url}`);
 });
