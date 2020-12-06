58fbab6351747c57d4fecf49d62455abd99ff828
Josh Justice <me@codingitwrong.com>
2020-12-06 10:36:42 -0500

    Allow toggling side nav on small

--- app/components/bootstrap-layout-container.js
+++ app/components/bootstrap-layout-container.js
@@ -1,6 +1,11 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class BootstrapLayoutContainerComponent extends Component {
   @tracked sideNavOpen = false;
+
+  @action toggleSideNavOpen() {
+    this.sideNavOpen = !this.sideNavOpen;
+  }
 }
