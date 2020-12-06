34250ce4647738b21120ad43d7e49a3f907e3656
Josh Justice <me@codingitwrong.com>
2020-12-06 07:34:18 -0500

    Fix icons on Safari

--- app/helpers/icon-mask-style.js
+++ app/helpers/icon-mask-style.js
@@ -1,5 +1,6 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function iconMaskStyle([icon]) {
-  return `mask-image: url(/icons/${icon}.svg)`;
+  const url = `url(/icons/${icon}.svg)`;
+  return `mask-image: ${url}; -webkit-mask-image: ${url}`;
 });
