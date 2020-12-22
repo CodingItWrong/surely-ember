a6716e8a5a9ff7e58de373a1093a7548fa800ec8
Josh Justice <me@codingitwrong.com>
2020-12-21 19:52:14 -0500

    Remove unneeded enter key handling

--- app/components/new-todo-form.js
+++ app/components/new-todo-form.js
@@ -4,8 +4,6 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { logRuntimeError } from 'surely/utils';
 
-const ENTER_KEY_CODE = 13;
-
 export default class NewTodoFormComponent extends Component {
   @service todos;
 
@@ -34,14 +32,5 @@ export default class NewTodoFormComponent extends Component {
       logRuntimeError(e);
       this.errorMessage = 'An error occurred adding the todo.';
     }
-  }
-
-  @action
-  handleNameKeyDown(e) {
-    if (e.keyCode === ENTER_KEY_CODE) {
-      this.createTodo();
-      return false;
-    }
-    return true;
   }
 }
