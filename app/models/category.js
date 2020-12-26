7acff714ea16bf647a450a798a3f3f1075825b06
Josh Justice <me@codingitwrong.com>
2020-12-25 20:09:13 -0500

    Add classic decorator to ED models

--- app/models/category.js
+++ app/models/category.js
@@ -1,5 +1,7 @@
 import Model, { attr } from '@ember-data/model';
+import classic from 'ember-classic-decorator';
 
+@classic
 export default class CategoryModel extends Model {
   @attr name;
   @attr sortOrder;
--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,7 @@
 import Model, { attr } from '@ember-data/model';
+import classic from 'ember-classic-decorator';
 
+@classic
 export default class UserModel extends Model {
   @attr email;
   @attr password;
