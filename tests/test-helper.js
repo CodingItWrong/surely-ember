317633494f1b06914a2821b195205dfd6df35863
Josh Justice <me@codingitwrong.com>
2021-01-06 08:00:48 -0500

    Update Ember to 3.24

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,11 +1,14 @@
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import setupSinon from 'ember-sinon-qunit';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
 import Application from 'surely/app';
 import config from 'surely/config/environment';
 
 setApplication(Application.create(config.APP));
 
+setup(QUnit.assert);
 setupSinon();
 
 start();
